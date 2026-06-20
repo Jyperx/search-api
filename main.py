a4ca08be0ff2147ad4841033732ecd38e48254c3
Simplify <playerhyperx57@gmail.com>
2026-06-20 02:49:22 -0500

    fix: CORS credentials para permitir peticiones desde React

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ app = FastAPI(title="Punto Search Engine (Mini-Algolia)")
 app.add_middleware(
     CORSMiddleware,
     allow_origins=["*"],
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
